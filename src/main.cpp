93008577ad1ca5160d19fb7213fdbd287335091e
stellar2000 <alterpro12@gmail.com>
2026-05-24 18:07:48 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,30 +12,32 @@ static void writeState(const char* state) {
 }
 
 class $modify(PlayLayer) {
-    bool m_levelStarted = false;
+    struct Fields {
+        bool m_levelStarted = false;
+    };
 
     void resetLevel() {
         PlayLayer::resetLevel();
-        m_levelStarted = false;
+        m_fields->m_levelStarted = false;
         writeState("RESET");
     }
 
     void startGame() {
         PlayLayer::startGame();
-        m_levelStarted = true;
+        m_fields->m_levelStarted = true;
         writeState("START");
     }
 
     void destroyPlayer(PlayerObject* p, GameObject* o) {
         PlayLayer::destroyPlayer(p, o);
-        if (m_levelStarted) {
+        if (m_fields->m_levelStarted) {
             writeState("DEATH");
         }
     }
 
     void onQuit() {
         PlayLayer::onQuit();
-        m_levelStarted = false;
+        m_fields->m_levelStarted = false;
         writeState("QUIT");
     }
 };
